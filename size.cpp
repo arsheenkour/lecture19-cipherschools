812c78b14f500a44c2f4eaa0c89f07d0444b9d56
arsheenkour <arsheenkour47@gmail.com>
2024-06-13 14:32:45 +0530

    first commit

---
+++ size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int a;//used when int is not sufficient
+	int b;
+	cout<<"The size of int is : "<<sizeof(int)<<endl;
+	cout<<"The size of float is : "<<sizeof(float)<<endl;
+	cout<<"The size of char is : "<<sizeof(char)<<endl;
+	cout<<"The size of  a is : "<<sizeof(a)<<endl;
+	cout<<"The size of b is : "<<sizeof(b)<<endl;
+	
+}
